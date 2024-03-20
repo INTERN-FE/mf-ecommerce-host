3fb5db0872e2d67ca10ba382990483f70140d6f0
aguswesly <agustchannel@gmail.com>
2024-03-20 08:29:34 +0700

    feat : add useCart hook

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,13 @@ import "./index.scss";
 import MainPage from "./pages/main-page";
 import AuthPage from "./pages/auth/auth-page";
 import BaseLayout from "./components/base-layout";
+import { useCart } from "./hooks/useCart";
 
 const App = () => {
+  const { state, dispatch } = useCart();
+
+  const cartItem = state.products;
+
   return (
     <HashRouter>
       <BaseLayout>
@@ -20,7 +25,12 @@ const App = () => {
           <Route path="/">
             <Route index element={<MainPage />} />
             <Route path="auth" element={<AuthPage />} />
-            <Route path="detail/:id" element={<DetailProduct />} />
+            <Route
+              path="detail/:id"
+              element={
+                <DetailProduct dispatch={dispatch} cartItem={cartItem} />
+              }
+            />
             <Route path="payment" element={<Payment />} />
           </Route>
         </Routes>
