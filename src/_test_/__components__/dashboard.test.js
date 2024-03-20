6ccf7e12601afd77a99cb4c5e66c00d88f013a9c
xyzuan <xyzuannihboss@gmail.com>
2024-03-20 13:35:27 +0700

    main(test): Add License header

--- src/_test_/__components__/dashboard.test.js
+++ src/_test_/__components__/dashboard.test.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright Intern MSIB6 @ PT Len Industri (Persero)
+ *
+ * THIS SOFTWARE SOURCE CODE AND ANY EXECUTABLE DERIVED THEREOF ARE PROPRIETARY
+ * TO PT LEN INDUSTRI (PERSERO), AS APPLICABLE, AND SHALL NOT BE USED IN ANY WAY
+ * OTHER THAN BEFOREHAND AGREED ON BY PT LEN INDUSTRI (PERSERO), NOR BE REPRODUCED
+ * OR DISCLOSED TO THIRD PARTIES WITHOUT PRIOR WRITTEN AUTHORIZATION BY
+ * PT LEN INDUSTRI (PERSERO), AS APPLICABLE.
+ *
+ * Created Date: Wednesday, March 20th 2024, 10:32:54 am
+ * Author: Jody Yuantoro | jodyyuan@xyzuan.my.id <https://github.com/xyzuan>
+ *
+ */
+
 import { MemoryRouter } from "react-router-dom";
 import { shallow, configure } from "enzyme";
 import Adapter from "@cfaester/enzyme-adapter-react-18";
--- src/_test_/__utils__/useCart.test.js
+++ src/_test_/__utils__/useCart.test.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright Intern MSIB6 @ PT Len Industri (Persero)
+ *
+ * THIS SOFTWARE SOURCE CODE AND ANY EXECUTABLE DERIVED THEREOF ARE PROPRIETARY
+ * TO PT LEN INDUSTRI (PERSERO), AS APPLICABLE, AND SHALL NOT BE USED IN ANY WAY
+ * OTHER THAN BEFOREHAND AGREED ON BY PT LEN INDUSTRI (PERSERO), NOR BE REPRODUCED
+ * OR DISCLOSED TO THIRD PARTIES WITHOUT PRIOR WRITTEN AUTHORIZATION BY
+ * PT LEN INDUSTRI (PERSERO), AS APPLICABLE.
+ *
+ * Created Date: Wednesday, March 20th 2024, 12:21:41 pm
+ * Author: Jody Yuantoro | jodyyuan@xyzuan.my.id <https://github.com/xyzuan>
+ *
+ */
+
 import { act } from "react-dom/test-utils";
 import { renderHook } from "@testing-library/react-hooks";
 
